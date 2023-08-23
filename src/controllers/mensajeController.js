b00a8b9e6abc732bee3be8536725b9e5cf9ab448
Mauro <mauro10z@hotmail.com>
2023-08-23 01:27:42 -0300

    Creacion controllers de mensajes

---
+++ src/controllers/mensajeController.js
@@ -0,0 +1,67 @@
+const Mensajes = require("../models/mensajes.model")
+
+
+//POST
+const createMessage = async (req,res)=>{
+  try {
+    const {nombre, email, mensaje} = req.body
+    const nuevoMensaje = new Mensajes({
+      nombre,
+      email,
+      mensaje
+    })
+    await nuevoMensaje.save()
+    res.status(201).json({message: "Mensaje Creado"})
+    
+  } catch (error) {
+    res.status(404).json({message:error.massage})
+  }
+}
+
+//GET
+const getMessage = async (req,res)=>{
+  try {
+    const allMessage = await Mensajes.find()
+    res.status(200).json(allMessage)
+
+  } catch (error) {
+    res.status(404).json({message: "Error al obtener los mensajes"})    
+  }
+}
+
+const getMessageByUser = async (req,res)=>{
+  try{
+    const userID = req.params.userId
+    const mensaje = await Mensajes.find({userID})
+    res.status(200).json(mensaje)
+  } catch(error){
+    res.status(500).json({message: "Error al obtener los mensajes del usuario"})
+  }
+}
+
+
+//DELETE
+const deleteMessage = async (req, res) => {
+  try {
+    const userId = req.params.userId
+    const messageId = req.params.messageId
+
+    // Buscar y borrar el mensaje solo si pertenece al usuario
+    const deletedMessage = await Mensajes.findOneAndDelete({ _id: messageId, userId });
+
+    if (!deletedMessage) {
+      return res.status(404).json({ message: "Mensaje no encontrado o no pertenece al usuario" });
+    }
+
+    res.status(200).json({ message: "Mensaje borrado exitosamente" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al borrar el mensaje" });
+  }
+};
+
+module.exports = {
+  createMessage,
+  getMessage,
+  getMessageByUser,
+  deleteMessage
+}
