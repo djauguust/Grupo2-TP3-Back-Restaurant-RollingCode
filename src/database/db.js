d659ea1b743248901ee55f41e53cb5b957010d76
djauguust <dj.august.brito@gmail.com>
2023-08-23 16:14:21 -0300

    agregado enlace para mongo atlas

--- src/database/db.js
+++ src/database/db.js
@@ -6,8 +6,9 @@ const db = process.env.DB;
 
 const conectDb = async () => {
   try {
-    await mongoose.connect(`${uri}${db}`, {
-      useNewUrlParser: true,
+    /* await mongoose.connect(`${uri}${db}`, { */
+    await mongoose.connect(`mongodb+srv://djauguust:FhOXKqYC4L5NzWWO@cluster0.jrw9sxt.mongodb.net/`, {
+    useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("DB is connected");
