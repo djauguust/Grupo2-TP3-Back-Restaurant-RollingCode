e8c439384490b3ba64ba8dd91a23732cbc0dfd29
Mauro <mauro10z@hotmail.com>
2023-08-23 01:32:16 -0300

    Enrutamiento de mensajes

--- src/routes/Mensajes.Routes.js
+++ src/routes/Mensajes.Routes.js
@@ -1,3 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
+const mensajesControllers = require("../controllers/mensajeController")
+
+//POST
+router.post("/mensjes", mensajesControllers.createMessage)
+
+//GET
+router.get("/mensjes", mensajesControllers.getMessage)
+router.get("/mensjes/:userId", mensajesControllers.getMessageByUser)
+
+//DELETE
+router.delete("/mensjes/:userId/:messageId", mensajesControllers.deleteMessage)
