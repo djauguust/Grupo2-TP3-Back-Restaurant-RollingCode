ebe72eb2dae1cb7035af0f3d21e890b50b497d3c
djauguust <dj.august.brito@gmail.com>
2023-08-29 20:42:24 -0300

    respuesta del getFechasND optimizado

--- src/controllers/fechasNoDisponiblesController.js
+++ src/controllers/fechasNoDisponiblesController.js
@@ -1,12 +1,39 @@
 const FechaNDModel = require("../models/fechasNoDisponibles.model");
 const FechasND = require("../models/fechasNoDisponibles.model");
+const Usuarios = require("../models/usuarios.model");
 
 //GET
 const getFechasNoDisponibles = async (req, res) => {
   try {
     const fechas = await FechasND.find();
+    const users = await Usuarios.find();
     // TO DO Hacer que solo devuelva las fechas presentes y futuras
-    res.status(200).json(fechas);
+    let aux = [];
+    let element;
+    fechas.map((f) => {
+      let user = users.find((u) => u._id == f.admin);
+      if (user) {
+        element = {
+          _id: f._id,
+          fecha: f.fecha,
+          admin: f.admin,
+          restaurant: f.restaurant,
+          nombre: user.nombre,
+          apellido: user.apellido,
+        };
+      } else {
+        element = {
+          _id: f._id,
+          fecha: f.fecha,
+          admin: f.admin,
+          restaurant: f.restaurant,
+          nombre: "No encontrado",
+          apellido: "No encontrado",
+        };
+      }
+      aux = [...aux, element];
+    });
+    res.status(200).json(aux);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -30,7 +57,7 @@ const newFechaND = async (req, res) => {
       res.status(201).json({ message: "¡Fecha No disponible Creada!" });
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -41,7 +68,6 @@ const deleteFecha = async (req, res) => {
     await FechasND.findOneAndDelete({ _id: id });
     res.status(200).json({ message: "Fecha No disponible eliminada" });
   } catch (error) {
-    console.log(error);
     res.status(404).json({ error: "Fecha no disponible no encontrada" });
   }
 };
